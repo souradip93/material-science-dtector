a26ed7192a718dbc616ef2cfc1737ddd87befa2d
Souradip Guha <souradip_pg@hnada.sit.iitkgp.ernet.in>
2020-05-06 23:19:40 +0530

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+pip install -r requirements.txt
+
+wget https://s3-eu-west-1.amazonaws.com/pfigshare-u-files/13773791/elmo_finetuned_matsci.tar.gz -O pytorch_lstmcrf-master/elmo_finetuned_matsci.tar.gz
+tar -xvzf pytorch_lstmcrf-master/elmo_finetuned_matsci.tar.gz
+
+wget https://www.dropbox.com/s/o3quow2k1x8wcee/all_mimicking_model_part2.tar.gz -O pytorch_lstmcrf-master/all_mimicking_model_part2/all_mimicking_model_part2.tar.gz
+
+
